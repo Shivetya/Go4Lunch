b5720566071e3ef8deff314e80acb6bfa1c3d37e
Shivetya <gthuillier2@gmail.com>
2019-08-09 13:01:13 +0200

    First commit after creation, add dependencies, androidX and Firebase.

---
+++ app/src/androidTest/java/com/gt/go4lunch/ExampleInstrumentedTest.kt
@@ -0,0 +1,25 @@
+package com.gt.go4lunch
+
+import android.content.Context
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.platform.app.InstrumentationRegistry
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext: Context = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.gt.go4lunch", appContext.packageName)
+    }
+}
