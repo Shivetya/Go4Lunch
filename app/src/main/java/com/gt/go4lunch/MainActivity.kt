fff89e994bd929a1f3a26eca8955a03928323958
Shivetya <gthuillier2@gmail.com>
2019-08-11 17:10:41 +0200

    Add authentication with mail, add style for authentication activity.

--- app/src/main/java/com/gt/go4lunch/MainActivity.kt
+++ app/src/main/java/com/gt/go4lunch/MainActivity.kt
@@ -2,11 +2,33 @@ package com.gt.go4lunch
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.firebase.ui.auth.AuthUI
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    companion object{
+        private const val ID_ACTIVITY_SIGN_IN = 1
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        activity_main_button_login_mail.setOnClickListener {
+            this.startLoginByMailActivity()
+        }
+    }
+
+    private fun startLoginByMailActivity(){
+
+        startActivityForResult(AuthUI.getInstance()
+            .createSignInIntentBuilder()
+            .setTheme(R.style.LoginTheme)
+            .setAvailableProviders(listOf(AuthUI.IdpConfig.EmailBuilder().build()))
+            .setIsSmartLockEnabled(false, true)
+            .setLogo(R.drawable.ic_launcher)
+            .build()
+        , ID_ACTIVITY_SIGN_IN)
     }
 }
