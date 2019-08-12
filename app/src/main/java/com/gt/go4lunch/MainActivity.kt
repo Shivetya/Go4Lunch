ec6c91cb0ffa16727dfdb8060d9a05caaf607deb
Shivetya <gthuillier2@gmail.com>
2019-08-12 10:52:46 +0200

    Add authentication with google and facebook.

--- app/src/main/java/com/gt/go4lunch/MainActivity.kt
+++ app/src/main/java/com/gt/go4lunch/MainActivity.kt
@@ -1,8 +1,12 @@
 package com.gt.go4lunch
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.firebase.ui.auth.AuthUI
+import com.firebase.ui.auth.ErrorCodes
+import com.firebase.ui.auth.IdpResponse
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
@@ -20,15 +24,40 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        handleWithActivityResult(requestCode, resultCode, data)
+    }
+
     private fun startLoginByMailActivity(){
 
         startActivityForResult(AuthUI.getInstance()
             .createSignInIntentBuilder()
             .setTheme(R.style.LoginTheme)
-            .setAvailableProviders(listOf(AuthUI.IdpConfig.EmailBuilder().build()))
+            .setAvailableProviders(listOf(AuthUI.IdpConfig.EmailBuilder().build()
+                ,AuthUI.IdpConfig.GoogleBuilder().build()
+                ,AuthUI.IdpConfig.FacebookBuilder().build()))
             .setIsSmartLockEnabled(false, true)
             .setLogo(R.drawable.ic_launcher)
             .build()
         , ID_ACTIVITY_SIGN_IN)
+    }
+
+    private fun handleWithActivityResult(requestCode: Int, resultCode: Int, data: Intent?){
+
+        val response: IdpResponse? = IdpResponse.fromResultIntent(data)
+
+        if (requestCode == ID_ACTIVITY_SIGN_IN){
+            if (resultCode == RESULT_OK){
+                //startActivityLogged()
+            } else {
+                when (response?.error?.errorCode){
+                    null -> Snackbar.make(activity_main_root, "Authentication Canceled", Snackbar.LENGTH_SHORT).show()
+                    ErrorCodes.NO_NETWORK -> Snackbar.make(activity_main_root, "No Internet !", Snackbar.LENGTH_SHORT).show()
+                    ErrorCodes.UNKNOWN_ERROR -> Snackbar.make(activity_main_root, "Unknown Error !", Snackbar.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
